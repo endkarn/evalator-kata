e9816fda1c2f23eecac239d43eec301b8ac19710
endkarn <end_karn@hotmail.com>
2019-08-20 11:55:59 +0700

    [Added]: UnitTest ของfunc CheckLiftControl

--- evalator/controller_test.go
+++ evalator/controller_test.go
@@ -1,1 +1,18 @@
 package evalator_test
+
+import "testing"
+
+func Test_LiftGoToFloor_Input_Goto_5_Current_1_Weight_100_Should_Be_Up(t *testing.T){
+	expected := "Up"
+	evaletor := evalator.Evalator{
+		GoToFloor:5,
+		Current:1,
+		Weight:100,
+	}
+
+	actual := checkLiftControl(evalator)
+
+	if expected != actual {
+		t.Errorf("Expected in %s but get %s",expected,actual)
+	}
+}
